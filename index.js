2e77a99121c5832f815604bd650ce8ad55f0581f
Minion3665 <pixtopia@null.net>
2019-05-24 11:02:37 +0100

    import edited barter

---
+++ index.js
@@ -0,0 +1,15 @@
+const webpack = require('webpack');
+const webpackconfig = require('./webpack.config.js');
+
+let compiler = webpack(webpackconfig);
+
+
+console.log("Beginning webpack build\n")
+compiler.run((err, stats) => {
+	if (stats.compilation.errors[0]) {
+		console.log("Webpack build failed\n");
+		console.log(stats.compilation.errors[0]);
+	} else {
+		console.log("Build finished, ready to deploy...");
+    }
+});
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var webpack = require('webpack');
+var path = require('path');
+var ProgressBarPlugin = require('progress-bar-webpack-plugin');
+var BUILD_DIR = path.resolve(__dirname, 'public');
+var APP_DIR = path.resolve(__dirname, 'client');
+
+var config = {
+	entry: APP_DIR + '/index.js',
+	mode: 'production',
+	output: {
+		path: BUILD_DIR,
+		filename: 'bundle.js'
+	},
+	plugins: [
+		new webpack.DefinePlugin({
+			'process.env': {
+				'NODE_ENV': JSON.stringify('production')
+			}
+		}),
+		new ProgressBarPlugin({
+  		format: '[:bar] :percent :msg',
+  		clear: false
+		})
+	],
+	module: {
+		rules: [
+			{
+				test: /\.js?/,
+				include: APP_DIR,
+				loader: 'babel-loader'
+			},
+		]
+	},
+};
+
+module.exports = config;
