08cd9ecff26dba053601127a40a8a9ec05e26886
Minion3665 <pixtopia@null.net>
2019-06-03 08:30:54 +0100

    Update index.js

--- client/index.js
+++ client/index.js
@@ -191,7 +191,7 @@ class MemberList extends Component {
 				}).map((member) => {
 					return (
 						<React.Fragment>
-							<Tag type={member.role}/> {member.name} {(member) => {
+							<Tag type={member.role}/> {member.name} {() => {
 								console.log("if")
 								if (member.specials) {
 									console.log("if passed")
