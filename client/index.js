b176a480a3170e1a506483f75735d8bf38b43579
Minion3665 <pixtopia@null.net>
2019-06-03 13:46:54 +0100

    Update index.js

--- client/index.js
+++ client/index.js
@@ -83,7 +83,7 @@ class Home extends Component {
 									specials: [
 										"admin"
 									]
-								}/*,
+								}*/,
 								{
 									role: "owner",
 									name: "DaSavMode1",
@@ -191,7 +191,7 @@ class MemberList extends Component {
 				}).map((member) => {
 					return (
 						<React.Fragment>
-							<Tag type={member.role}/> {member.name} {() => {
+							<Tag type={member.role}/> {member.name} {(member) => {
 								console.log("if")
 								if (member.specials) {
 									console.log("if passed")
