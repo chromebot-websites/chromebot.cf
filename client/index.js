50abbc55bf598509f53f0d6aa623259fba312be4
Minion3665 <pixtopia@null.net>
2019-06-04 21:19:44 +0100

    Update index.js

--- client/index.js
+++ client/index.js
@@ -288,7 +288,7 @@ class MemberList extends Component {
 					}
 				};
 				return (
-					<span className="disable-select tooltip" data-tooltip={specials[special].description} style={{fontSize: 80%}}>{specials[special].emoji}</span>
+					<span className="disable-select tooltip" data-tooltip={specials[special].description} style={{fontSize: "80%"}}>{specials[special].emoji}</span>
 				);
 			});
 		}
@@ -322,28 +322,28 @@ class Tag extends Component {
 	constructor(props) {
 		super(props);
 		let tagTypes = {
-			"founder":{"title":"Founder of Chromebook777 Productions", "color":"#070000"},
-			"owner":{"title":"Owner", "color":"#007C1D"},
-			"manager":{"title":"Staff Management", "color":"#992D22"},
-			"admin":{"title":"Admin", "color":"#187E04"},
-			"headdev":{"title":"Head of Development", "color":"#FF9600"},
-			"headbotdev":{"title":"Head of Bot Development", "color":"#FF0000"},
-			"headwebdev":{"title":"Head of Website Development", "color":"#9B59B6"},
-			"mod":{"title":"Moderator", "color":"#A84300"},
-			"jmod":{"title":"Junior-Moderator", "color":"#A84300"},
-			"tmod":{"title":"Trial-Moderator", "color":"#00EEFF"}
+			"founder":{"title":"Founder of Chromebook777 Productions", "background-color":"#070000", "color":"#ffffff"},
+			"owner":{"title":"Owner", "background-color":"#007C1D", "color":"#ffffff"},
+			"manager":{"title":"Staff Management", "background-color":"#992D22", "color":"#ffffff"},
+			"admin":{"title":"Admin", "background-color":"#187E04", "color":"#ffffff"},
+			"headdev":{"title":"Head of Development", "background-color":"#FF9600", "color":"#000000"},
+			"headbotdev":{"title":"Head of Bot Development", "background-color":"#FF0000", "color":"#000000"},
+			"headwebdev":{"title":"Head of Website Development", "background-color":"#9B59B6", "color":"#000000"},
+			"mod":{"title":"Moderator", "background-color":"#A84300", "color":"#ffffff"},
+			"jmod":{"title":"Junior-Moderator", "background-color":"#A84300", "color":"#ffffff"},
+			"tmod":{"title":"Trial-Moderator", "background-color":"#00EEFF", "color":"#ffffff"}
 		}
 		if (props.color && props.title) {
 			this.state = {color: props.color, title: props.title};
 		} else if (props.type && props.type in tagTypes) {
 			this.state = {color: tagTypes[props.type].color, title: tagTypes[props.type].title};
 		} else {
-			this.state = {color: "#000000", title: "DEFAULT"};
+			this.state = {"background-color": "#ffffff", title: "DEFAULT", "color":"#000000"};
 		}
 	}
 	render() {
 		return (
-			<b style={{color: this.state.color}}>[{this.state.title}]</b>
+			<b style={{color: this.state.color, backgroundColor: this.state["background-color"], borderRadius: "10px"}}>[{this.state.title}]</b>
 		);
 	}
 }
