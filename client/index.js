b02a8eec1635c29abafd085dd780fcd91667953b
Minion3665 <pixtopia@null.net>
2019-06-05 07:25:59 +0100

    Update index.js

--- client/index.js
+++ client/index.js
@@ -288,7 +288,7 @@ class MemberList extends Component {
 					}
 				};
 				return (
-					<span className="disable-select tooltip" data-tooltip={specials[special].description} style={{fontSize: "80%"}}>{specials[special].emoji}</span>
+					<span className="disable-select tooltip" data-tooltip={specials[special].description}>{specials[special].emoji}</span>
 				);
 			});
 		}
@@ -343,7 +343,7 @@ class Tag extends Component {
 	}
 	render() {
 		return (
-			<b style={{color: this.state.color, backgroundColor: this.state["background-color"], borderRadius: "5px", display: "inline-block", padding: "0.3vw", margin:"0.15vw"}}>{this.state.title}</b>
+			<b style={{color: this.state.color, backgroundColor: this.state["background-color"], borderRadius: "5px", display: "inline-block", padding: "0.4vw", margin:"0.2vw"}}>{this.state.title}</b>
 		);
 	}
 }
