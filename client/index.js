b2e670d77b5462f6547d6f1bbae47f66b09d894e
Minion3665 <pixtopia@null.net>
2019-06-06 17:29:24 +0100

    Update index.js

--- client/index.js
+++ client/index.js
@@ -343,7 +343,7 @@ class Tag extends Component {
 	}
 	render() {
 		return (
-			<b style={{color: this.state.color, backgroundColor: this.state["background-color"], borderRadius: "5px", display: "inline-block", padding: "0.4vw", margin:"0.2vw"}}>{this.state.title}</b>
+			<b style={{color: this.state.color, backgroundColor: this.state["background-color"], borderRadius: "5px", display: "inline-block", padding: "10px", margin:"5px"}}>{this.state.title}</b>
 		);
 	}
 }
