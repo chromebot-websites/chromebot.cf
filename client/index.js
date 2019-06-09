1af5e6582c4289dabc4751ee9910037d453b2a70
Minion3665 <pixtopia@null.net>
2019-06-09 07:18:30 +0100

    Update index.js

--- client/index.js
+++ client/index.js
@@ -66,7 +66,7 @@ class ErrorBoundary extends Component {
 					<div className="page one variant2">
 						<div className="textblock">Hey, that's an Error.<br />While trying to fetch this resource on chromebot.cf there was a bug</div>
 						<Button important onClick={(button) => window.location.href = "/"}>Back To Home</Button>
-						<Button destructive onClick={(button) => window.location.href = "https://github.com/chromebook777-productions/chromebot.cf/issues"}>Report a bug</Button>
+						<Button destructive onClick={(button) => window.location.href = "https://github.com/chromebook777-productions/chromebot.cf/issues"}>Report bug</Button>
 					</div>
 					<div className="page two variant2">
 						<div className="textblock">Website made by <a href="https://github.com/minion3665">@Minion3665</a> on github</div>
@@ -245,7 +245,7 @@ class Error404 extends Component {
 				<div className="page one variant2">
 					<div className="textblock">Hey, that's a 404.<br />That resource was not found on chromebot.cf.</div>
 					<Button important onClick={(button) => window.location.href = "/"}>Back To Home</Button>
-					<Button destructive onClick={(button) => window.location.href = "https://github.com/chromebook777-productions/chromebot.cf/issues"}>Report a bug</Button>
+					<Button destructive onClick={(button) => window.location.href = "https://github.com/chromebook777-productions/chromebot.cf/issues"}>Report a bug if you believe this is in issue</Button>
 				</div>
 				<div className="page two variant2">
 					<div className="textblock">Website made by <a href="https://github.com/minion3665">@Minion3665</a> on github</div>
