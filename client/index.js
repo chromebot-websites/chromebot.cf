7ebc7d957ec640306e00dc54d47bcdb3992eedf4
Minion3665 <pixtopia@null.net>
2019-06-09 17:43:08 +0100

    Update index.js

--- client/index.js
+++ client/index.js
@@ -264,7 +264,7 @@ class OnlineOffline extends Component {
 	}
 	componentDidMount() {
     var discordWidget = new XMLHttpRequest();
-		discordWidget.onreadystatechange = function() {
+		discordWidget.onreadystatechange = (this) => {
 				if (this.readyState == 4 && this.status == 200) {
 					 // Typical action to be performed when the document is ready:
 					let vinceOn = false;
