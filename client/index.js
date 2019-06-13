5d1f33a8e2f1703d604c7f0b02ad8cfad4f9d98d
Minion3665 <pixtopia@null.net>
2019-06-13 18:09:55 +0100

    Update index.js

--- client/index.js
+++ client/index.js
@@ -1,11 +1,10 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import { React, Component } from 'react';
+import { ReactDOM } from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Error from "./pages/error.js";
-import HomePage from "./premade/home.js";
-import ErrorBoundary from "./utils/errorBoundaries.js";
-import Button from "./utils/button.js";
-import MemberList, { Tag } from "./utils/members.js";
+import { Error } from "./pages/error.js";
+import { HomePage } from "./premade/home.js";
+import { ErrorBoundary } from "./utils/errorBoundaries.js";
+import { Button } from "./utils/button.js";
 
 let buildNumber = 0.23;
 
@@ -15,26 +14,24 @@ console.log("Welcome to the chromebot website. This is client build B." + buildN
 class App extends Component {
 	render() {
 		return (
-			<SuperErrorBoundary>
-				<ErrorBoundary>
-					<Router>
-						<Switch>
-							<Route exact path="/" component={HomePage} />
-							<Route path="/join" render={() => {
-								window.location.href = "https://discordapp.com/invite/77NM8VQ";
-								return (
-									<React.Fragment>
-										<div className="textblock">We're redirecting you now...</div>
-										<Button important onClick={(button) => window.location.href = "https://discordapp.com/invite/77NM8VQ"}>Not Being Redirected?</Button>
-									</React.Fragment>
-								);
-							}} />
-							<Route path="/vince" component={OnlineOffline} />
-							<Route render={() => {return(<Error code="404" description="The requested resource was not found on the chromebot support website"/>)}} />
-						</Switch>
-					</Router>
-				</ErrorBoundary>
-			</SuperErrorBoundary>
+			<ErrorBoundary>
+				<Router>
+					<Switch>
+						<Route exact path="/" component={HomePage} />
+						<Route path="/join" render={() => {
+							window.location.href = "https://discordapp.com/invite/77NM8VQ";
+							return (
+								<React.Fragment>
+									<div className="textblock">We're redirecting you now...</div>
+									<Button important onClick={(button) => window.location.href = "https://discordapp.com/invite/77NM8VQ"}>Not Being Redirected?</Button>
+								</React.Fragment>
+							);
+						}} />
+						<Route path="/vince" component={OnlineOffline} />
+						<Route render={() => {return(<Error code="404" description="The requested resource was not found on the chromebot support website"/>)}} />
+					</Switch>
+				</Router>
+			</ErrorBoundary>
 		);
 	}
 }
