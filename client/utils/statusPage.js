4e5d68e5ded4903b485174b3e941ad384db81068
Minion3665 <pixtopia@null.net>
2019-06-15 10:50:27 +0100

    Update statusPage.js

--- client/utils/statusPage.js
+++ client/utils/statusPage.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
 
 class StatusPage extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {color: "grey", message: "Getting the latest data for <span className='highlight'>"+props.botname+"</span>"};
+	}
 	render () {
-		return null;
+		return (
+			<React.Fragment>
+				<div className="page statusPage" style={backgroundColor: this.state.color, backgroundImage: "linear-gradient(to bottom, "+this.props.prevColor+" 0%, "+this.state.color+" 10%, "+this.state.color+" 90%, "+this.props.nextColor+" 100%)"}>
+					<b>{this.state.message}</b>
+				</div>
+			</React.Fragment>
+		);
 	}
 }
 
