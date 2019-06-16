c8a8b393afe249ae2331a8ade7028469bfebccfc
Minion3665 <pixtopia@null.net>
2019-06-16 09:00:48 +0100

    Update statusPage.js

--- client/utils/statusPage.js
+++ client/utils/statusPage.js
@@ -37,20 +37,13 @@ class StatusPage extends Component {
           this.setState({ color: "red" });
           this.setState({ message: "Offline" });
         }
-        setTimeout(this.checkStatus, 5000);
+        setTimeout(() => {
+          this.state.xmlhttp.open("GET", "https://discordapp.com/api/guilds/" + this.props.serverId + "/widget.json?timestamp=" + new Date().getTime(), true); //we appecnd the current timestamp to bypass caching, it's hacky but it works. Please don't remove it unless you have a better solution.
+          this.state.xmlhttp.send();
+        }, 5000);
       }
     };
-    this.checkStatus();
-  }
-  checkStatus() {
-    this.state.xmlhttp.open(
-      "GET",
-      "https://discordapp.com/api/guilds/" +
-        this.props.serverId +
-        "/widget.json?timestamp=" +
-        new Date().getTime(),
-      true
-    ); //we append the current timestamp to bypass caching, it's hacky but it works. Please don't remove it unless you have a better solution.
+    this.state.xmlhttp.open("GET", "https://discordapp.com/api/guilds/" + this.props.serverId + "/widget.json?timestamp=" + new Date().getTime(), true); //we append the current timestamp to bypass caching, it's hacky but it works. Please don't remove it unless you have a better solution.
     this.state.xmlhttp.send();
   }
   componentWillUnmount() {
