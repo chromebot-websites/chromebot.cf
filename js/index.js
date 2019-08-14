96867e99ca89b695640b9d45f5c18b1a74283d2b
Minion3665 <34243578+Minion3665@users.noreply.github.com>
2019-08-14 13:39:40 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,8 +3,9 @@ var sticky = logo.offsetTop;
 
 function stick() {
   if (window.pageYOffset >= sticky) {
-    logo.parentElement.childNodes.forEach (child) => {
+    logo.parentElement.childNodes.forEach((child) => {
       child.classList.add("sticky");
+    });
     logo.parentElement.classList.add("stickyContainer");
   } else {
     logo.parentElement.childNodes.forEach((child) => {
