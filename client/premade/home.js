b118a9738b6a4eaa0938201e8582c6e068a1efd5
Minion3665 <34243578+Minion3665@users.noreply.github.com>
2019-08-17 09:29:27 +0100

    Update home.js

--- client/premade/home.js
+++ client/premade/home.js
@@ -1,70 +1,14 @@
 import React, { Component } from "react";
-import Button from "./../utils/button.js";
+import Navbar from "./../utils/navbar.js";
 import Status from "./../utils/status.js";
 import PercentBar from "./../utils/percentBar.js";
 import { MemberList } from "./../utils/members.js";
 
 class HomePage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      logo: null,
-      sticky: null,
-      allowStick: false
-    };
-  }
-  stick() {
-    if (!this.state.allowStick) {
-      return;
-    }
-    console.log(window.pageYOffset);
-    if (window.pageYOffset >= this.state.sticky) {
-      console.log("Adding classes");
-      this.state.logo.parentElement.childNodes.forEach(child => {
-        child.classList.add("sticky");
-      });
-      document.body.classList.add("sticky");
-      this.state.logo.parentElement.classList.add("stickyContainer");
-    } else {
-      console.log("Removing classes");
-      this.state.logo.parentElement.childNodes.forEach(child => {
-        child.classList.remove("sticky");
-      });
-      document.body.classList.remove("sticky");
-      this.state.logo.parentElement.classList.remove("stickyContainer");
-    }
-  }
-  componentDidUpdate() {
-    this.stick();
-  }
-  componentDidMount() {
-    this.setState({
-      logo: document.getElementById("logo"),
-      sticky: document.getElementById("logo").offsetTop,
-      allowStick: true
-    });
-    window.onscroll = () => {
-      this.stick();
-    };
-  }
   render() {
     return (
       <React.Fragment>
-        <div>
-          <img
-            src="images/faviconRotate.GIF"
-            className="logo"
-            id="logo"
-            alt="the chromebot logo"
-          />
-          <Button
-            important
-            onClick={button => (window.location.href = "/join")}
-          >
-            Join
-          </Button>
-          <Button disabled>Help and information</Button>
-        </div>
+        <Navbar/>
         <PercentBar
           serverId="480959345601937410"
           searchForMembers={[
