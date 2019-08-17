04aa8976bc8d7bf3d7e4d6de950cd4079fe25562
Minion3665 <34243578+Minion3665@users.noreply.github.com>
2019-08-17 09:40:09 +0100

    Update navbar.js

--- client/utils/navbar.js
+++ client/utils/navbar.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import Button from "./button.js";
 
 class Navbar extends Component {
-  constuctor(props) {
+  constructor(props) {
     super(props);
     this.state = {
       logo: null,
