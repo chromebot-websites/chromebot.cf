8d476d8f14e0b2ac3a9c611616061c0907ae7d03
Minion3665 <34243578+Minion3665@users.noreply.github.com>
2019-08-18 10:45:40 +0100

    Update navbar.js

--- client/utils/navbar.js
+++ client/utils/navbar.js
@@ -50,16 +50,18 @@ class Navbar extends Component {
           <Button
             important
             onClick={button =>
-          (
+          {
             window.location.href = "https://discordapp.com/invite/77NM8VQ";
             button.unpress();
-          )}>
+          }}>
             Join
           </Button>
-          <Button onClick={button => (
+          <Button 
+            onClick={button =>
+          {
             window.location.href = "#bot-invite";
             button.unpress();
-          )}>
+          }}>
             Help and information
           </Button>
         </div>
