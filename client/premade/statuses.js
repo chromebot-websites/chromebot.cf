7cbcc3bce629de199348d4c66c2c1816ebb54c98
Restyled.io <commits@restyled.io>
2019-08-23 07:38:48 +0000

    Restyled by prettier

--- client/premade/statuses.js
+++ client/premade/statuses.js
@@ -2,36 +2,36 @@ import React, { Component } from "react";
 import Status from "./../utils/status.js";
 
 class Statuses extends Component {
-	render () {
-		return (
-			<React.Fragment>
-				<div className="centerContainer">
-					<Status
-						botName="Chromebot"
-						botId="499262934715727872"
-						serverId="480959345601937410"
-					/>
-					<Status
-						botName="Chromebot Canary"
-						botId="555361766947815424"
-						serverId="480959345601937410"
-					/>
-					</div>
-					<div className="centerContainer">
-					<Status
-						botName="Chromebot ModMail"
-						botId="587060114318688256"
-						serverId="480959345601937410"
-					/>
-					<Status
-						botName="Chromebot Manager"
-						botId="599743179608293398"
-						serverId="480959345601937410"
-					/>
-				</div>
-			</React.Fragment>
-		);
-	}
+  render() {
+    return (
+      <React.Fragment>
+        <div className="centerContainer">
+          <Status
+            botName="Chromebot"
+            botId="499262934715727872"
+            serverId="480959345601937410"
+          />
+          <Status
+            botName="Chromebot Canary"
+            botId="555361766947815424"
+            serverId="480959345601937410"
+          />
+        </div>
+        <div className="centerContainer">
+          <Status
+            botName="Chromebot ModMail"
+            botId="587060114318688256"
+            serverId="480959345601937410"
+          />
+          <Status
+            botName="Chromebot Manager"
+            botId="599743179608293398"
+            serverId="480959345601937410"
+          />
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default Statuses;
