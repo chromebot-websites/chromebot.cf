c7263fb7123876013adb4ac08e4309796a115509
Minion3665 <34243578+Minion3665@users.noreply.github.com>
2019-08-23 08:33:10 +0100

    Update bots.js

--- client/premade/bots.js
+++ client/premade/bots.js
@@ -19,30 +19,7 @@ class Bots extends Component {
             //"317731855317336067", // Minion3665 (for testing only)
           ]}
         />
-        <div className="centerContainer">
-          <Status
-            botName="Chromebot"
-            botId="499262934715727872"
-            serverId="480959345601937410"
-          />
-          <Status
-            botName="Chromebot Canary"
-            botId="555361766947815424"
-            serverId="480959345601937410"
-          />
-        </div>
-        <div className="centerContainer">
-          <Status
-            botName="Chromebot ModMail"
-            botId="587060114318688256"
-            serverId="480959345601937410"
-          />
-          <Status
-            botName="Chromebot Manager"
-            botId="599743179608293398"
-            serverId="480959345601937410"
-          />
-        </div>
+        <Statuses />
         <a id="bot-invite" />
         <b className="title">Invite the bots</b>
         <Button
