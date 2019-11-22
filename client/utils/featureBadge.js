ee3d7d86fda855fed79385af98f922db7b5e0aae
Minion3665 <34243578+Minion3665@users.noreply.github.com>
2019-11-22 21:27:24 +0000

    Update featureBadge.js

--- client/utils/featureBadge.js
+++ client/utils/featureBadge.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 
 class Badge extends Component {
 	render() {
-		<div style={width: "25%", backgroundImage: "linear-gradient(white, #ffffff33)", fontWeight: "bolder", fontSize: "3vw"}>
-			<div style={fontSize: "10vw"}>{this.props.icon}</div>
+		<div style={{width: "25%", backgroundImage: "linear-gradient(white, #ffffff33)", fontWeight: "bolder", fontSize: "3vw"}}>
+			<div style={{fontSize: "10vw"}}>{this.props.icon}</div>
 			{this.props.title}
-			<div style={fontSize: "1vw"}>{this.props.description}</div>
+			<div style={{fontSize: "1vw"}}>{this.props.description}</div>
 		</div>
 	}
 }
