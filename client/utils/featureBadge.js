4386a7cf26badc73a149d094601d0553a37c0b99
Restyled.io <commits@restyled.io>
2019-11-22 21:54:40 +0000

    Restyled by prettier

--- client/utils/featureBadge.js
+++ client/utils/featureBadge.js
@@ -1,22 +1,22 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 class Badge extends Component {
   render() {
     return (
       <div
-    style =
-        {
-          {
-            width: "25%", backgroundImage: "linear-gradient(white, #ffffff33)",
-                fontWeight: "bolder", fontSize: "3vw", marginLeft: "2vw",
-                marginRight: "2vw",
-          }
-        } >
-        <div style = {{ fontSize: "10vw" }}>{this.props.icon}<
-            /div>
+        style={{
+          width: "25%",
+          backgroundImage: "linear-gradient(white, #ffffff33)",
+          fontWeight: "bolder",
+          fontSize: "3vw",
+          marginLeft: "2vw",
+          marginRight: "2vw"
+        }}
+      >
+        <div style={{ fontSize: "10vw" }}>{this.props.icon}</div>
         {this.props.title}
-        <div style={{ fontSize: "1vw" }}>{this.props.description}</div><
-        /div>
+        <div style={{ fontSize: "1vw" }}>{this.props.description}</div>
+      </div>
     );
   }
 }
