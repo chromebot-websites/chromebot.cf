eb8e1bbf256e9726c6f729af45c5a4e3077e9b4e
Restyled.io <commits@restyled.io>
2019-11-23 14:39:35 +0000

    Restyled by clang-format

--- client/utils/featureBadge.js
+++ client/utils/featureBadge.js
@@ -1,25 +1,23 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 
 class Badge extends Component {
   render() {
     return (
       <div
-        style={{
-          width: "25%",
-          backgroundImage: "linear-gradient(white, #ffffff55)",
-          fontWeight: "bolder",
-          fontSize: "3vw",
-          margin: "2vw",
-          textAlign: "center",
-          borderRadius: "1vw",
-          minHeight: "30vw",
-          padding: "1vw",
-        }}
-      >
-        <div style={{ fontSize: "10vw" }}>{this.props.icon}</div>
+    style =
+        {
+          {
+            width: "25%", backgroundImage: "linear-gradient(white, #ffffff55)",
+                fontWeight: "bolder", fontSize: "3vw", margin: "2vw",
+                textAlign: "center", borderRadius: "1vw", minHeight: "30vw",
+                padding: "1vw",
+          }
+        } >
+        <div style = {{ fontSize: "10vw" }}>{this.props.icon}<
+            /div>
         {this.props.title}
-        <div style={{ fontSize: "1vw" }}>{this.props.description}</div>
-      </div>
+        <div style={{ fontSize: "1vw" }}>{this.props.description}</div><
+        /div>
     );
   }
 }
