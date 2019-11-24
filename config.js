0192176eb8b1c7a3ac89425415b9bb9d688e5959
Minion3665 <pixtopia@null.net>
2019-11-24 14:48:24 +0000

    Updates

--- config.js
+++
@@ -1,7 +0,0 @@
-if (process.env.NODE_ENV != 'production') require('dotenv');
-
-module.exports = {
-	APPNAME: process.env.APPNAME || 'ChromeBot Support Website',
-	PORT: process.env.PORT || 3000,
-	DEVMODE: (process.env.NODE_ENV != 'production'),
-};
