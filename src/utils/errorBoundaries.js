9d9ad50cb2f68f73025cedbfa80587fabf3e7478
Restyled.io <commits@restyled.io>
2019-12-05 11:35:38 +0000

    Restyled by prettier

--- src/utils/errorBoundaries.js
+++ src/utils/errorBoundaries.js
@@ -1,45 +1,45 @@
-import React, {Component} from 'react';
-import Error from './../pages/error.js';
+import React, { Component } from "react";
+import Error from "./../pages/error.js";
 
 class ErrorBoundary extends Component {
   constructor(props) {
     super(props);
-    this.state = {error : null};
+    this.state = { error: null };
   }
-  componentDidCatch(error, errorInfo) { this.setState({error}); }
+  componentDidCatch(error, errorInfo) {
+    this.setState({ error });
+  }
   render() {
     if (this.state.error) {
-      return ("AHHH PANIC!!!")
-      return (
-          "Hi. I'm Minion3665. If you can see this then the website is dead. Properly dead. Please create a new issue at https://github.com/chromebook777-productions/chromebot.cf/issues with the title 'I got a supererror'. I am not going to link anywhere for fear of breaking stuff further, as if this error boundary breaks then I have nothing left. If you want to go back to the main page then go to https://chromebot.cf.Thanks, Minion3665/");
+      return "AHHH PANIC!!!";
+      return "Hi. I'm Minion3665. If you can see this then the website is dead. Properly dead. Please create a new issue at https://github.com/chromebook777-productions/chromebot.cf/issues with the title 'I got a supererror'. I am not going to link anywhere for fear of breaking stuff further, as if this error boundary breaks then I have nothing left. If you want to go back to the main page then go to https://chromebot.cf.Thanks, Minion3665/";
     } else {
-                        return (
-				<WeakErrorBoundary>
-					{this.props.children}
-				</WeakErrorBoundary>
-			);
-		}
-	}
+      return <WeakErrorBoundary>{this.props.children}</WeakErrorBoundary>;
+    }
+  }
 }
 class WeakErrorBoundary extends Component {
-	constructor(props) {
-		super(props);
-		this.state = { error: null };
-	}
-	componentDidCatch(error, errorInfo) {
-		this.setState({ error });
-	}
-	render() {
-		if (this.state.error) {
-			return (
-				<React.Fragment>
-					<Error code="601" description="An unknown error occured. Who knows what this one means..."/>
-				</React.Fragment>
-			);
-		} else {
-			return this.props.children;
-		}
-	}
+  constructor(props) {
+    super(props);
+    this.state = { error: null };
+  }
+  componentDidCatch(error, errorInfo) {
+    this.setState({ error });
+  }
+  render() {
+    if (this.state.error) {
+      return (
+        <React.Fragment>
+          <Error
+            code="601"
+            description="An unknown error occured. Who knows what this one means..."
+          />
+        </React.Fragment>
+      );
+    } else {
+      return this.props.children;
+    }
+  }
 }
 
 export default ErrorBoundary;
