feff0ce023e1d5fecfde832a7cd59f50c7eb4882
Minion <34243578+Minion3665@users.noreply.github.com>
2019-12-05 20:37:09 +0000

    Update error.js

--- src/pages/error.js
+++ src/pages/error.js
@@ -10,14 +10,14 @@ class Error extends Component {
 		return (
 			<React.Fragment>
 				<div className="page one variant2">
-					<div className="textblock">Hey, that's an error {this.state.errorType}<br />{this.state.errorMessage}</div>
+					<div className="textblock">Hey, that\'s an error {this.state.errorType}<br />{this.state.errorMessage}</div>
 					<Button important onClick={(button) => {window.location.href = "/";}}>Back To Home</Button>
 					<Button destructive onClick={(button) => {window.location.href = "https://github.com/chromebot-websites/chromebot.cf/issues";}}>Report a bug if you believe this is in issue</Button>
 				</div>
 				<div className="page two variant2">
 					<div className="textblock variant2">Website made by <a href="https://github.com/minion3665">@Minion3665</a> on github</div>
 					<div className="textblock variant2">Discord server made by <a href="https://github.com/chromebook777">@Chromebook777</a> on github</div>
-					<div className="textblock variant2">Minon's slave <a href="https://github.com/vilagamer999">@Vilagamer9999</a> On github</div>
+					<div className="textblock variant2">Minon\'s slave <a href="https://github.com/vilagamer999">@Vilagamer9999</a> On github</div>
 				</div>
 			</React.Fragment>
 		);
