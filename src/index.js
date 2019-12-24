efb48c900f8bcd469399bfd0137ac88bca4914d1
Restyled.io <commits@restyled.io>
2019-12-24 18:39:01 +0000

    Restyled by clang-format

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import "./css/base.css";
 import "./css/custom.css";
 
-import React, { Component } from "react";
+import React, {Component} from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 import Error from "./pages/error.js";
 import Bots from "./premade/bots.js";
@@ -13,11 +13,8 @@ import ErrorBoundary from "./utils/errorBoundaries.js";
 
 let buildNumber = 0.01;
 
-console.log(
-  "Welcome to the chromebot website. This is client build BR." +
-    buildNumber +
-    "."
-);
+console.log("Welcome to the chromebot website. This is client build BR." +
+            buildNumber + ".");
 
 class App extends Component {
   render() {
@@ -25,11 +22,13 @@ class App extends Component {
       <ErrorBoundary>
         <Router>
           <Switch>
-            <Route exact path="/" component={HomePage} />
+            <Route exact path="/" component={
+      HomePage} />
 
             <Route
               path="/christmas"
-              render={() => {
+    render = {
+      () => {
                 return (
                   <React.Fragment>
                     <Button important>
@@ -37,11 +36,11 @@ class App extends Component {
                     </Button>
                     <Button special>
                       <span role="img">🎅</span> Would like to wish you a merry
-                      christmas! <span role="img">🎅</span>
+                christmas!
+                    <span role = "img">🎅</span>
                     </Button>
-                    <br />
-                    <Button
-                      destructive
+                    <br />< Button
+                destructive
                       onClick={button =>
                         (window.location.href = "https://chromebot.cf")
                       }
@@ -50,31 +49,32 @@ class App extends Component {
                     </Button>
                   </React.Fragment>
                 );
-              }}
-            />
+      }
+    } />
 
             <Route
               path="/status "
-              render={() => {
+    render = {
+      () => {
                 return (
                   <React.Fragment>
                     <Button
-                      destructive
-                      onClick={button =>
-                        (window.location.href = "https://chromebot.cf")
-                      }
-                    >
-                      Go Back Home
-                    </Button>
+                destructive
+                onClick =
+                    {button =>
+                         (window.location.href = "https://chromebot.cf")} >
+                    Go Back Home</Button>
                     <Bots />
-                  </React.Fragment>
+                    </React.Fragment>
                 );
               }}
-            />
-            <Route
-              path="/join"
-              render={() => {
-                window.location.href = "https://discordapp.com/invite/77NM8VQ";
+            /><
+                    Route
+                path = "/join"
+                render = {
+                  () => {
+                    window.location.href =
+                        "https://discordapp.com/invite/77NM8VQ";
                 return (
                   <React.Fragment>
                     <div className="textblock">
@@ -85,32 +85,30 @@ class App extends Component {
                       onClick={button =>
                         (window.location.href =
                           "https://discordapp.com/invite/77NM8VQ")
-                      }
-                    >
-                      {" "}
-                      Not Being Redirected ?{" "}
-                    </Button>
-                  </React.Fragment>
-                );
-              }}
-            />
+                  } > {" "} Not Being Redirected
+                      ? {" "}</Button>
+                  </React.Fragment>);
+                }
+      } />
             <Route
               path="/egg "
               render={() => {
-                return (
-                  <React.Fragment>
-                    <Button destructive>Yay you found an easter egg...</Button>
+        return (<React.Fragment>
+                <Button destructive>Yay you found an easter egg...<
+                    /Button>
                     <Button destructive>Not much to see here though...</Button>
-                    <Button special>:)</Button>
-                    <Button important> idk lol</Button>
-                    <Button
-                      ugly
-                      onClick={button =>
-                        (window.location.href =
-                          "https://raw.githubusercontent.com/chromebot-websites/chromebot.cf/master/public/images/2AA15818-B987-4253-AA6B-74BBE74173D6.jpeg")
-                      }
-                    >
-                      {" "}
+                <Button special>:)<
+                   /Button>
+                    <Button important> idk lol</Button><
+               Button
+        ugly
+        onClick =
+            {button => (
+                 window.location.href =
+                     "https://raw.githubusercontent.com/chromebot-websites/chromebot.cf/master/public/images/2AA15818-B987-4253-AA6B-74BBE74173D6.jpeg")} >
+        {
+          " "
+        }
                       10/10 best button
                     </Button>
                   </React.Fragment>
@@ -125,13 +123,14 @@ class App extends Component {
                     description="The requested resource was not found on the chromebot support website"
                   />
                 );
-              }}
+    }
+  }
             />
           </Switch>
         </Router>
       </ErrorBoundary>
     );
-  }
+}
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
