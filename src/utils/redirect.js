5691901f3513e6178b6228620dfe0332024505f1
Vilagamer999 <30276916+Vilagamer999@users.noreply.github.com>
2020-01-07 10:30:55 +0000

    Create redirect.js

---
+++ src/utils/redirect.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      
+<script>
+     // Internet Explorer 6-11
+          const isIE = document.documentMode;
+          if (isIE){
+            window.alert(
+              "Your MESSAGE here."
+            )
+          }
+</script>
+
+    );
+  }
+}
+
+export default App;
