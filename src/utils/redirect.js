5fd34f3fdd322cad420c624079198d9f105326b0
Restyled.io <commits@restyled.io>
2020-01-07 10:31:19 +0000

    Restyled by prettier

--- src/utils/redirect.js
+++ src/utils/redirect.js
@@ -1,17 +1,12 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 class App extends Component {
   render() {
     return (
-      
-<script>
-     // Internet Explorer 6-11
-          const isIE = document.documentMode;
-          if (isIE){
-      window.alert("Your MESSAGE here.")
-          }
-</script>
-
+      <script>
+        // Internet Explorer 6-11 const isIE = document.documentMode; if (isIE)
+        {window.alert("Your MESSAGE here.")}
+      </script>
     );
   }
 }
